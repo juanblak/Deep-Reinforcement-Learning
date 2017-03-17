8b1baa9271d4be6726fcc95e7362a893101bde5e
Jane <459333694@qq.com>
2017-03-17 17:21:53 +0900

    Update maze_env.py

--- maze_env.py
+++ maze_env.py
@@ -1,17 +1,3 @@
-"""
-Reinforcement learning maze example.
-
-Red rectangle:          explorer.
-Black rectangles:       hells       [reward = -1].
-Yellow bin circle:      paradise    [reward = +1].
-All other states:       ground      [reward = 0].
-
-This script is the environment part of this example.
-The RL is in RL_brain.py.
-
-View more on 莫烦Python: https://morvanzhou.github.io/tutorials/
-"""
-
 
 import numpy as np
 import tkinter as tk
